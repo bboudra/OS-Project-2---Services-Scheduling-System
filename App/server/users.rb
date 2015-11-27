6ffff7a4f084160962e0d350c4d2acabfd4fd872
Benjamin <Benjamin@gmail.com>
2015-11-27 00:13:25 -0700

    Version 0.0.10: user does not exist test case written and passing. Writing a test for users to see if list is updated when add is called. Also writing a user class

--- App/server/users.rb
+++ App/server/users.rb
@@ -1,11 +1,15 @@
 require 'singleton'
 class Users
   include Singleton
+
   def initialize
     @user_hash = {}
   end
 
-  def user_that_exists?
-
+  ##
+  #@param username, the name of the user
+  def user_that_exists?(username)
+    return @user_hash.has_key?(username)
   end
+
 end
--- spec/server/users_spec.rb
+++ spec/server/users_spec.rb
@@ -2,12 +2,16 @@ require 'rspec'
 require_relative '../../App/server/users'
 
 describe Users do
-  def initialize
-    @users = Users.new
+  before(:each) do
+    @users = Users.instance
   end
-  context 'The user should not be in the list of users' do
+
+  context 'The user is not in the list of users' do
     it 'Should say the user does not exist when asked' do
-      @users.should_not be_an_user_that_exists()
+      expect(@users).not_to be_user_that_exists(:user_that_does_not_exist)
+    end
+
+    it 'should be added tot he list of users' do
     end
   end
 end
