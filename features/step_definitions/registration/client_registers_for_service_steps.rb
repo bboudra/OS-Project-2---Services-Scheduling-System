5eb5ed25901f02ae1336079c331862775f14c27c
Benjamin <Benjamin@gmail.com>
2015-11-26 21:21:05 -0700

    Version 0.0.5: Temporarily removed scenarios that are not presently being worked on, created a step definitions file

---
+++ features/step_definitions/registration/client_registers_for_service_steps.rb
@@ -0,0 +1,21 @@
+
+Given(/^I want to register for an appointment$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+And(/^The date\-time combination is past the current date\-time$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^The appointment will not overlap with an appointment that already exists$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I register for an appointment$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will receive a message saying that the registration was successful$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
